5bf0f877475c20abcdaf95af6a2b882c7cbca9c1
Anna Spysz <spysz@amazon.com>
2024-02-01 16:14:39 -0800

    Update function languages

--- src/GetRepo/index.js
+++ src/GetRepo/index.js
@@ -1,48 +1,6 @@
-const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-const { DynamoDBDocumentClient, GetCommand } = require("@aws-sdk/lib-dynamodb");
-
-const client = new DynamoDBClient({});
-const ddbDocClient = DynamoDBDocumentClient.from(client);
-
 exports.handler = async event => {
-  try {
-    // Log the event argument for debugging and for use in local development.
-    console.log(JSON.stringify(event, undefined, 2));
+  // Log the event argument for debugging and for use in local development.
+  console.log(JSON.stringify(event, undefined, 2));
 
-    const id = event.pathParameters.id;
-
-    console.log(`Getting item with ID '${id}' from table '${({}).ITEMS_TABLE_NAME}'`);
-
-    const command = new GetCommand({
-      TableName: ({}).ITEMS_TABLE_NAME,
-      Key: { id }
-    });
-
-    const response = await ddbDocClient.send(command);
-
-    if (response.Item) {
-      console.log(`Retrieved item '${id}' with attributes ${JSON.stringify(response.Item)}`);
-
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          item: response.Item
-        })
-      };
-    } else {
-      console.log(`No item found with id '${id}'`);
-
-      return {
-        statusCode: 404,
-        body: "Item not found"
-      };
-    }
-  } catch (e) {
-    console.error(`Failed to get item: ${e.message} (${e.constructor.name})`);
-
-    return {
-      statusCode: 500,
-      body: "Internal Service Error"
-    };
-  }
+  return {};
 };
