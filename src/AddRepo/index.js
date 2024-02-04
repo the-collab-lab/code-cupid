1dffd561441545c4d4331fa14bb93b1106aa965f
Danielle Heberling <dheberling@ensomata.com>
2024-02-04 10:39:18 -0800

    Code for AddRepo and GetRepo lambdas

--- src/AddRepo/index.js
+++ src/AddRepo/index.js
@@ -1,6 +1,52 @@
-exports.handler = async event => {
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, PutCommand } = require("@aws-sdk/lib-dynamodb");
+
+exports.handler = async (event) => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
+  const data = JSON.parse(event.body);
 
-  return {};
+  if (!data.repoURL || !data.language) {
+    return {
+      statusCode: 400,
+      body: "repoURL and language are required fields",
+    };
+  }
+
+  const extractData = data.repoURL.replace(/(^\w+:|^)\/\//, "").split("/");
+  const repoOwner = extractData[1];
+  const repoName = extractData[2];
+
+  if (!repoName || !repoOwner) {
+    return {
+      statusCode: 400,
+      body: "Invalid repoURL",
+    };
+  }
+
+  const repoId = `${repoOwner}-${repoName}`;
+
+  const input = {
+    id: repoId,
+    link: data.repoURL,
+    language: data.language,
+    createdAt: new Date().getTime().toString(),
+  };
+
+  const params = {
+    TableName: process.env.REPOS_TABLE_NAME,
+    Item: input,
+  };
+
+  const client = new DynamoDBClient({ region: process.env.AWS_REGION });
+  const ddbDocClient = DynamoDBDocumentClient.from(client);
+
+  console.log(`Adding repo ${repoId} to table ${process.env.REPOS_TABLE_NAME}`);
+  await ddbDocClient.send(new PutCommand(params));
+  console.log("Repo added to table, done");
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(input),
+  };
 };
--- src/GetRepo/index.js
+++ src/GetRepo/index.js
@@ -1,6 +1,41 @@
-exports.handler = async event => {
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, GetCommand } = require("@aws-sdk/lib-dynamodb");
+
+exports.handler = async (event) => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
 
-  return {};
+  const params = {
+    TableName: process.env.REPOS_TABLE_NAME,
+    Key: { id: event.pathParameters.id },
+  };
+
+  let response;
+  let statusCode;
+  try {
+    const client = new DynamoDBClient({ region: process.env.AWS_REGION });
+    const ddbDocClient = DynamoDBDocumentClient.from(client);
+
+    const { Item } = await ddbDocClient.send(new GetCommand(params));
+
+    if (!Item) {
+      statusCode = 404;
+      response = {
+        message: `Repo with id ${event.pathParameters.id} not found`,
+      };
+    } else {
+      response = Item;
+      statusCode = 200;
+      console.log(`Success getting repo: ${event.pathParameters.id}`);
+    }
+  } catch (err) {
+    console.log(`Error: ${JSON.stringify(err, undefined, 2)}`);
+    response = { message: err.message };
+    statusCode = err.$metadata ? err.$metadata.httpStatusCode : 500;
+  }
+
+  return {
+    statusCode,
+    body: JSON.stringify(response),
+  };
 };
